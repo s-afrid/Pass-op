814d1b32a816968e695f3ec6e008eb67f47a8ff3
Syed Afrid <83272741+s-afrid@users.noreply.github.com>
2025-11-30 16:11:18 +0530

    added background using tailwind backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Navbar from "./components/Navbar"
+import Manager from "./components/Manager"
 function App() {
   return (
     <>
     <Navbar />
-      <div>
-        This is my UI
-      </div>
+    <Manager />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-purple-200 flex justify-around items-center px-4'>
+    <nav className='bg-purple-200 flex justify-around items-center px-4 h-14'>
         <div className="logo font-bold">Pass-Op</div>
       <ul>
         <li className='flex gap-4'>
