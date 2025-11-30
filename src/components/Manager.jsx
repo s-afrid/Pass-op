27ac05090f6009029a99f3313c733f47a3c2b506
Syed Afrid <83272741+s-afrid@users.noreply.github.com>
2025-11-30 17:56:46 +0530

    added input fields for managing

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -8,15 +8,21 @@ const Manager = () => {
 
         <div className="bg-slate-50 mycontainer">
 
-            <h1>Pass-Op</h1>
-            <p>Your own password manager</p>
+            <h1 className='text-4xl font-bold text-center'>
+              <span className='text-green-700'>&lt; </span>
+        <span>Pass</span>
+        <span className='text-green-500'> Op</span>
+        <span className='text-green-700'> /&gt;</span>
+            </h1>
+            <p className='text-green-900 text-lg text-center'>Your own password manager</p>
 
-            <div className="flex flex-col p-4">
-            <input className='bg-white rounded-full' type="text" name="" id="" />
-            <div className="flex">
-                <input className='bg-white rounded-full' type="text" />
-                <input className='bg-white rounded-full' type="text" />
+            <div className="flex flex-col p-4 gap-5">
+            <input className='bg-white rounded-full border border-green-500 w-full px-4 py-1' type="text" name="" id="" />
+            <div className="flex w-full justify-between gap-4">
+                <input className='bg-white rounded-full border border-green-500 w-full px-4 py-1' type="text" />
+                <input className='bg-white rounded-full border border-green-500 w-full px-4 py-1' type="text" />
             </div>
+            <button>Add Password</button>
         </div>
 
         </div>
