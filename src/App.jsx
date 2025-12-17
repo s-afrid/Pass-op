974903c439e1a5d8fe883f5f1d801cc992f90cfb
s-afrid <afrid0052@gmail.com>
2025-12-17 16:33:49 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar"
 import Manager from "./components/Manager"
+import Footer from "./components/Footer"
 function App() {
   return (
     <>
     <Navbar />
     <Manager />
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-800 text-white flex justify-around items-center h-14 w-full sticky bottom-0'>
+        <div className="logo font-bold text-2xl">
+        <span className='text-green-500'>&lt; </span>
+        <span className='text-white'>Pass</span>
+        <span className='text-green-400'> Op</span>
+        <span className='text-green-500'> /&gt;</span>
+        </div>
+        <div className='flex'> Created with &nbsp;<img className='w-5' src='icons/heart.svg' alt='love' />&nbsp; by Syed Afrid</div>
+     
+    </div>
+  )
+}
+
+export default Footer
