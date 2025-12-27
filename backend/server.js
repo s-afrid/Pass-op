3a9c8d3baa028d13f4d18a130fe2be0deaa24105
s-afrid <afrid0052@gmail.com>
2025-12-27 15:34:29 +0530

    installed all modules like express and mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,31 @@
 const express = require('express')
+
+require('dotenv').config()
+const { MongoClient } = require('mongodb')
+const url = process.env.MONGO_URL
+const client = new MongoClient(url)
+const dbname = 'passop'
+
 const app = express()
 const port = 3000
+client.connect();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// get all passwords
+app.get('/', async (req, res) => {
+    const db = client.db(dbname)
+    const collection = db.collection('documents')
+    const findResult = await collection.find({}).toArray();
+  res.json(findResult)
+})
+
+//save passwords
+app.post('/', async (req, res) => {
+
+})
+
+//delete passwords
+app.delete('/', async (req, res) => {
+    
 })
 
 app.listen(port, () => {
