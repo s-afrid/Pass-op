84b6462b7f9ec0b30b00cf49524099f9efdf4576
s-afrid <afrid0052@gmail.com>
2025-12-27 16:04:25 +0530

    insert, delete, and get api's are created

--- backend/server.js
+++ backend/server.js
@@ -2,31 +2,44 @@ const express = require('express')
 
 require('dotenv').config()
 const { MongoClient } = require('mongodb')
+const bodyParser = require('body-parser')
+
 const url = process.env.MONGO_URL
 const client = new MongoClient(url)
 const dbname = 'passop'
 
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
 client.connect();
 
 // get all passwords
 app.get('/', async (req, res) => {
     const db = client.db(dbname)
-    const collection = db.collection('documents')
+    const collection = db.collection('passwords')
     const findResult = await collection.find({}).toArray();
   res.json(findResult)
 })
 
 //save passwords
 app.post('/', async (req, res) => {
-
+    const password = req.body
+    const db = client.db(dbname)
+    const collection = db.collection('passwords')
+    const findResult = await collection.insertOne(password)
+    res.json({success: true, message: findResult})
 })
 
 //delete passwords
 app.delete('/', async (req, res) => {
-    
+    const password = req.body
+    const db = client.db(dbname)
+    const collection = db.collection('passwords')
+    const findResult = await collection.deleteOne(password)
+    res.json({success: true, message: findResult})
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
